34fcb0e72b26e1c9aa8bfb666a0cd6316c7eff03
Gustavo <gustavo_hen@hotmail.com>
2024-12-21 13:19:44 -0300

    - Cleanup controllers + Application tokens

--- client/src/proxy.conf.js
+++ client/src/proxy.conf.js
@@ -1,7 +1,10 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7168';
+const target = (
+  env.ASPNETCORE_HTTPS_PORT ?
+    `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    (env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7168')
+);
 
 const PROXY_CONFIG = [
   {
--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,10 @@
+using Server.Business.Services;
+using Server.Data.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddSingleton<PasswordServiceInterface, PasswordService>();
+builder.Services.AddSingleton<TokenServiceInterface, TokenService>();
 
 builder.Services.AddControllers();
 
@@ -8,15 +12,9 @@ var app = builder.Build();
 
 app.UseDefaultFiles();
 app.MapStaticAssets();
-
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
-
 app.Run();
