4cb677cc70391368625a9331e61e68f9cbc0262e
Gustavo <gustavo_hen@hotmail.com>
2024-12-21 13:06:30 -0300

    + xUnit Dependencies + Password / Token Service Tests * Organization for rest server calls

---
+++ server/Test/Services/PasswordServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using Server.Business.Services;
+using Server.Data.Interfaces;
+using Xunit.Abstractions;
+
+namespace Server.Test.Services;
+
+public sealed class PasswordServiceTest
+{
+    private readonly PasswordServiceInterface _sut;
+    private readonly ITestOutputHelper _logger;
+    private readonly int _maxLength;
+
+    // Arrange
+    public PasswordServiceTest(ITestOutputHelper output)
+    {
+        _sut = new PasswordService();
+        _logger = output;
+        _maxLength = 8;
+    }
+
+    [Fact]
+    public void Generate_Eight_Password_Length()
+    {
+        // Act
+        var pwd = _sut.Generate();
+        _logger.WriteLine(pwd);
+
+        // Assert
+        Assert.NotNull(pwd);
+        Assert.NotEmpty(pwd);
+        Assert.True(pwd.Length.Equals(_maxLength));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("     ")]
+    [InlineData("12345678")]
+    [InlineData("abcdefgh")]
+    [InlineData("ABCDEFGH")]
+    [InlineData("@@@@@@@@")]
+    [InlineData("aB3")]
+    [InlineData("aB@")]
+    [InlineData("1234")]
+    [InlineData("abcd1234")]
+    [InlineData("ABCD1234")]
+    [InlineData("abcd@#$%")]
+    [InlineData("ABCD@#$%")]
+    public void Invalid_Password_Input(string data)
+    {
+        // Act
+        var isValid = _sut.Validate(data);
+
+        // Assert
+        Assert.False(isValid);
+    }
+
+    [Theory]
+    [InlineData("A2b1g@4=")]
+    [InlineData("5g)A!H1b")]
+    [InlineData("A1@bC2$dEf3")]
+    public void Valid_Password_Input(string data)
+    {
+        // Act
+        var isValid = _sut.Validate(data);
+
+        // Assert
+        Assert.True(isValid);
+    }
+}
---
+++ server/Test/Services/TokenServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Server.Business.Services;
+using Server.Data.Interfaces;
+using Xunit.Abstractions;
+using Server.Data.Enums;
+using Moq;
+
+namespace Server.Test.Services;
+
+public sealed class TokenServiceTest
+{
+    private readonly TokenServiceInterface _sut;
+    private readonly Mock<IConfiguration> _config;
+    private readonly ITestOutputHelper _logger;
+
+    // Arrange
+    public TokenServiceTest(ITestOutputHelper output)
+    {
+        _config = new Mock<IConfiguration>();
+
+        _config
+            .Setup(c => c["Secret"]) // Dev version
+            .Returns("aB3dF8gH9jK1lM2nP0qR5tS7uV6wXyZ4");
+
+        _sut = new TokenService(_config.Object);
+
+        _logger = output;
+    }
+
+    [Theory]
+    [InlineData(RoleEnum.Viewer, "J3PkxKv8YvwUMK8uaZY6cQ==")]
+    [InlineData(RoleEnum.Coordinator, "G7gYZqy3nHpraJ9FCCfoBQ==")]
+    [InlineData(RoleEnum.Administrator, "Kh5O/naBc2cLy11qucBpsg==")]
+    public void Generate_Token_By(RoleEnum role, string content)
+    {
+        // Act
+        var token = _sut.Generate(role);
+        _logger.WriteLine(token);
+
+        // Assert
+        Assert.NotNull(token);
+        Assert.NotEmpty(token);
+        Assert.Equal(token, content);
+    }
+}
